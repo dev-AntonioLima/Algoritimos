9754abae07708552afb97361083f83a7cd47e578
Antonio Lima <alimamg4@gmail.com>
2020-06-03 12:38:48 -0300

    JS

---
+++ Javascript/FunçãoJS/Resultado.js
@@ -0,0 +1,7 @@
+function calcula() {
+      var n1 = parseInt(document.getElementById('valor1').value);
+      var n2 = parseInt(document.getElementById('valor2').value);
+      soma=n1+n2;
+      document.write("<p>Soma: " + soma + "</p>");
+
+     }
